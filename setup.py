a46afa3a9d0cdbe7fe8a31fb7eb25f9cb4b98ff8
John James <john.james@nov8.ai>
2021-10-25 22:49:41 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Lab for software development experiments and best practices',
+    author='John James',
+    license='BSD-3',
+)
